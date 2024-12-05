836f6c9c5a829801dfeae3ccc54ed943e35b21ae
ALAN DAVID DA COSTA VARELA <20211094040031@ifrn.local>
2024-12-05 16:05:31 -0300

    Criar Atividae

---
+++ exercicio/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Produto
+
+admin.site.register(Produto)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('cod_produto', 'nome_produto', 'preco', 'qntd_estoque','data_criacao')
+
+    search_fields = ('cod_produto', 'nome')
+    ordering = ('data_criacao',)
